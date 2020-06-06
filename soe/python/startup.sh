c4b8e2c7329eb0ef5fdc9f6e64633df7c72273cd
JK <kineticsquid@gmail.com>
2020-06-05 20:55:56 -0400

    Resolving PR comments

--- soe/python/startup.sh
+++ soe/python/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CLOUDANT_URL="https://{credentials}:e7833b543725775f89ff1f0915c1f67135423a2d5b7746588ce99c2f9b72dc06@cb3fca4e-e0f6-4a7b-9a98-f3c8ed9d4820-bluemix.cloudant.com"
+export CLOUDANT_URL="https://{cloudant_account}:{password}@{cloudant_account}-bluemix.cloudant.com"
 export CONVERSATION_PASSWORD="password"
 export CONVERSATION_USERNAME="username"
 export CONVERSATION_VERSION="2017-04-21"
